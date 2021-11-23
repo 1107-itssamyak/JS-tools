549c0960daef0b2d34199cb7cf0f93d76b8da82e
1107-itssamyak <samyak.mehta11@gmail.com>
2021-11-23 21:27:22 +0530

    updating Image Carousel (timing function and button functioning)

--- Image Carousel/script.js
+++ Image Carousel/script.js
@@ -1,21 +1,18 @@
-const leftbutton = document.getElementById("left");
-const rightbutton = document.getElementById("right");
-const centerbutton = document.getElementById("center");
+const leftbutton = document.querySelector("#left");
+const rightbutton = document.querySelector("#right");
+const centerbutton = document.querySelector("#center");
 const icontain = document.querySelector('#imgs');
-const message = document.getElementById("message");
+const message = document.querySelector("#message");
 
 // setting intervals for moving the images here using setInterval
-let interval = setInterval(run, 5000);
+let interval = setInterval(run, 4000);
 
 function run() {
     cnt++;
     changeImage();
 }
 
-function changeImage() {
-    if (cnt < 1) cnt = 4;
-    else if (cnt > 4) cnt = 1;
-
+function remover(cnt) {
     icontain.classList.remove("carousel1");
     icontain.classList.remove("carousel2");
     icontain.classList.remove("carousel3");
@@ -24,44 +21,9 @@ function changeImage() {
     icontain.classList.add(`carousel${cnt}`);
 }
 
-let cnt = 1;
-
-// adding onclick eventlisteners
-rightbutton.addEventListener('click', () => {
-    cnt++;
-    if (cnt > 4) cnt = 1;
-
-    icontain.classList.remove("carousel1");
-    icontain.classList.remove("carousel2");
-    icontain.classList.remove("carousel3");
-    icontain.classList.remove("carousel4");
-
-    icontain.classList.add(`carousel${cnt}`);
-    clearInterval(interval);
-    setTimeout(() => {
-        setInterval(run, 5000);
-    }, 500);
-})
-
-leftbutton.addEventListener('click', () => {
-    cnt--;
-    if (cnt < 1) cnt = 4;
-
-    icontain.classList.remove("carousel1");
-    icontain.classList.remove("carousel2");
-    icontain.classList.remove("carousel3");
-    icontain.classList.remove("carousel4");
-
-    icontain.classList.add(`carousel${cnt}`);
-    clearInterval(interval);
-    setTimeout(() => {
-        setInterval(run, 5000);
-    }, 500);
-})
-
-centerbutton.addEventListener('click', () => {
+function intervalCheckerPrimary() {
     if (interval === null) {
-        interval = window.setInterval(run, 5000);
+        interval = window.setInterval(run, 4000);
         centerbutton.innerHTML = `<i class="fas fa-pause"></i>`;
         message.innerText = "The Carousel Slider is on auto play";
     } else {
@@ -70,4 +32,41 @@ centerbutton.addEventListener('click', () => {
         message.innerText = "The Carousel Slider is currently paused";
         interval = null;
     }
+}
+
+function intervalCheckerSecondary() {
+    window.clearInterval(interval);
+    centerbutton.innerHTML = `<i class="fas fa-play"></i>`;
+    message.innerText = "The Carousel Slider is currently paused";
+    interval = null;
+}
+
+function changeImage() {
+    if (cnt < 1) cnt = 4;
+    else if (cnt > 4) cnt = 1;
+
+    remover(cnt);
+}
+
+let cnt = 1;
+
+//TODO adding onclick eventlisteners
+rightbutton.addEventListener('click', () => {
+    cnt++;
+    if (cnt > 4) cnt = 1;
+
+    remover(cnt);
+    intervalCheckerSecondary();
+})
+
+leftbutton.addEventListener('click', () => {
+    cnt--;
+    if (cnt < 1) cnt = 4;
+
+    remover(cnt);
+    intervalCheckerSecondary();
+})
+
+centerbutton.addEventListener('click', () => {
+    intervalCheckerPrimary();
 })
